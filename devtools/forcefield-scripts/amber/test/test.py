df4ce734fdecaf5dba5e29d3f8fbc2ad4c263939
Rafal P. Wiewiora <rafwiewiora@gmail.com>
2016-03-14 18:35:17 -0400

    update script name in test

--- devtools/forcefield-scripts/amber/test/test.py
+++ devtools/forcefield-scripts/amber/test/test.py
@@ -6,13 +6,13 @@ class Testamber2ommmScript(unittest.TestCase):
 
     def test_leaprc(self):
         """ Test conversion of a leaprc"""
-        cmd = ('python amber2omm.py -i test/leaprc.ff14SB -if leaprc -od test/ffxml '
+        cmd = ('python amber2openmm.py -i test/leaprc.ff14SB -if leaprc -od test/ffxml '
                '--no-log --protein-test --nucleic-test')
         os.system(cmd)
 
     def test_yaml(self):
         """ Test conversion of a short yaml"""
-        cmd = ('python amber2omm.py -i test/test.yaml -od test/ffxml --no-log')
+        cmd = ('python amber2openmm.py -i test/test.yaml -od test/ffxml --no-log')
         os.system(cmd)
 
 if __name__ == '__main__':
